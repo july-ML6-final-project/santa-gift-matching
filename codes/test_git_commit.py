082748b4e55d4ffdc5993e180781ec45567159e9
xshuai <shuaixin.david@gmail.com>
2018-12-17 01:07:36 -0600

    create a codes directory and a test commit .py file

---
+++ codes/test_git_commit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Describe Module Functionality
+"""
+
+# import standard module #
+import sys
+
+
+# import third-party module #
+# import user defined module #
+
+# define global directories #
+
+# define common data/directories #
+
+# define functions #
+
+
+def main(args):
+    """Main entry point allowing external calls
+    """
+    print()
+
+
+def run():
+    """Entry point for console_scripts
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    run()
